c0e32b2060fe524c8b0900bea74a50bfa1addfb7
Bogdan Olar <olar.bogdan.dev@gmail.com>
2023-03-05 09:31:15 +0200

    Fixed bug where we were callinf the `step()~ method on the spot rather than on `self`

--- src/lib.rs
+++ src/lib.rs
@@ -128,10 +128,8 @@ impl Minefield {
                 // only try to autostep if the user has placed enough flags around the spot whose neighbors will be autorevealed
                 if placed_flags == neighboring_mines {
                     for (nx, ny) in self.neighbors_coords(x, y) {
-                        let step_result = self.field.get_mut(&(nx, ny)).unwrap().step();
-
-                        if StepResult::Boom == step_result {
-                            return step_result;
+                        if StepResult::Boom == self.step(nx, ny) {
+                            return StepResult::Boom;
                         }
                     }
                 }
